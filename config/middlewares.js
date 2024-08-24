ca329c6246b373f324150197528753f94e32d5c6
Manjesh Singh <smanjesh1854@gmail.com>
2024-08-24 11:18:44 +0530

    config changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,15 +2,7 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:3000', 'https://tychr.pages.dev/'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['*'],
-      credentials: true
-    }
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
