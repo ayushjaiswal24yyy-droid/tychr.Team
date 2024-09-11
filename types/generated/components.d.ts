75589d5b30b03765f18075208c4f4abc4b8c718a
Manjesh Singh <smanjesh1854@gmail.com>
2024-09-11 18:29:18 +0530

    removeed content

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -28,6 +28,19 @@ export interface SubtopicHeading extends Schema.Component {
   };
 }
 
+export interface SubjectRefrenceBooks extends Schema.Component {
+  collectionName: 'components_subject_refrence_books';
+  info: {
+    displayName: 'Refrence Books';
+    icon: 'book';
+  };
+  attributes: {
+    title: Attribute.String;
+    author: Attribute.String;
+    publication_year: Attribute.Integer;
+  };
+}
+
 export interface PlanGradePlan extends Schema.Component {
   collectionName: 'components_plan_grade_plans';
   info: {
@@ -45,26 +58,13 @@ export interface PlanGradePlan extends Schema.Component {
   };
 }
 
-export interface SubjectRefrenceBooks extends Schema.Component {
-  collectionName: 'components_subject_refrence_books';
-  info: {
-    displayName: 'Refrence Books';
-    icon: 'book';
-  };
-  attributes: {
-    title: Attribute.String;
-    author: Attribute.String;
-    publication_year: Attribute.Integer;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
       'subtopic.qn-a': SubtopicQnA;
       'subtopic.heading': SubtopicHeading;
-      'plan.grade-plan': PlanGradePlan;
       'subject.refrence-books': SubjectRefrenceBooks;
+      'plan.grade-plan': PlanGradePlan;
     }
   }
 }
