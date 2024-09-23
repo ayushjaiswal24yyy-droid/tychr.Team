bd753e55e052564185a15a580e80dc3b826f193e
Manjesh Singh <smanjesh1854@gmail.com>
2024-09-23 18:34:32 +0530

    cookie data

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -74,7 +74,7 @@ module.exports = (plugin) => {
                         { username: identifier }
                     ],
                 },
-                populate: ['role', 'fav_topics', 'avatar', 'onBoarded', 'ib_program', 'grade'],
+                populate: ['role', 'fav_topics', 'avatar', 'onBoarded', 'ib_program', 'grade', 'teaching', 'enrollments'],
             });
 
             if (!user) {
