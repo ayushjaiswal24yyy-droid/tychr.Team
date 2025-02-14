286e5de1a969cda4bcf3f5de4994790c15abb674
Miran Hamid <miranhamid2002@gmail.com>
2025-02-14 16:24:09 +0530

    add socket io for realtime chat

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+  async create(ctx) {
+
+    const response = await super.create(ctx);
+
+    if (strapi.io) {
+      const newPost = response.data; // Extract the newly created post
+      strapi.io.emit('newPost', newPost); // Broadcast the new post
+    }
+
+    return response;
+  },
+}));
