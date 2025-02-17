fe6f7a09e9e21db720ec1ca119a8f2829c139e23
Miran Hamid <miranhamid2002@gmail.com>
2025-02-17 13:34:28 +0530

    update middlware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,5 +1,5 @@
+const externalURI = "www.wiris.net";
 
-const externalURI = "www.wiris.net"
 module.exports = ({ env }) => [
   'strapi::errors',
   {
@@ -7,14 +7,21 @@ module.exports = ({ env }) => [
     config: {
       contentSecurityPolicy: {
         directives: {
-          'script-src': ["'self'","'unsafe-eval'", "'unsafe-inline'",  externalURI],
+          'script-src': ["'self'", "'unsafe-eval'", "'unsafe-inline'", externalURI],
           'script-src-attr': ["'self'", "'unsafe-inline'", externalURI],
-          'font-src': ["'self'",  externalURI, "https://fonts.gstatic.com", "https://cdnjs.cloudflare.com"],
+          'font-src': ["'self'", externalURI, "https://fonts.gstatic.com", "https://cdnjs.cloudflare.com"],
           'connect-src': ["'self'", externalURI, "https://*.strapi.io"],
           'style-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", externalURI, "https://fonts.googleapis.com", "https://cdnjs.cloudflare.com", "http://cdn.jsdelivr.net"],
-          'img-src': ["'self'", "blob:", "data:", externalURI, "https://*.strapi.io"],
+          'img-src': [
+            "'self'", 
+            "blob:", 
+            "data:", 
+            externalURI, 
+            "https://*.strapi.io", 
+            "https://m.media-amazon.com" // Add this line
+          ],
         },
-      }
+      },
     },
   },
   'strapi::cors',
@@ -24,4 +31,4 @@ module.exports = ({ env }) => [
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
-];     
+];
