a42d4ef23b0182cf7907a3335ff1914d1854b0ff
Miran Hamid <miranhamid2002@gmail.com>
2025-02-19 23:39:54 +0530

    enhance post creation to broadcast fully populated post details

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,21 +1,24 @@
-'use strict';
-
+"use strict";
 /**
  * post controller
  */
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
   async create(ctx) {
-
     const response = await super.create(ctx);
 
+    const populatedPost = await strapi.entityService.findOne(
+      "api::post.post",
+      response.data.id,
+      {
+        populate: ["author", "attachment", "community"],
+      }
+    );
+
     if (strapi.io) {
-      const newPost = response.data; // Extract the newly created post
-      strapi.io.emit('newPost', newPost); // Broadcast the new post
-      console.log('New post broadcasted:', newPost);
-      
+      strapi.io.emit("newPost", populatedPost); // Broadcast the fully populated post
+      console.log("New post broadcasted:", populatedPost);
     }
 
     return response;
