d2c3dcde2a7e885b51e5fa7ef81af20881d00e14
Miran Hamid <miranhamid2002@gmail.com>
2025-07-05 23:42:06 +0530

    refactor: standardize quotes in middleware configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,35 +1,60 @@
 const externalURI = "www.wiris.net";
 
 module.exports = ({ env }) => [
-  'strapi::errors',
+  "strapi::errors",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         directives: {
-          'script-src': ["'self'", "'unsafe-eval'", "'unsafe-inline'", externalURI],
-          'script-src-attr': ["'self'", "'unsafe-inline'", externalURI],
-          'font-src': ["'self'", externalURI, "https://fonts.gstatic.com", "https://cdnjs.cloudflare.com"],
-          'connect-src': ["'self'", externalURI, "https://*.strapi.io"],
-          'style-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'", externalURI, "https://fonts.googleapis.com", "https://cdnjs.cloudflare.com", "http://cdn.jsdelivr.net"],
-          'img-src': [
-            "'self'", 
-            "blob:", 
-            "data:", 
-            externalURI, 
-            "https://*.strapi.io", 
+          "script-src": [
+            "'self'",
+            "'unsafe-eval'",
+            "'unsafe-inline'",
+            externalURI,
+          ],
+          "script-src-attr": ["'self'", "'unsafe-inline'", externalURI],
+          "font-src": [
+            "'self'",
+            externalURI,
+            "https://fonts.gstatic.com",
+            "https://cdnjs.cloudflare.com",
+          ],
+          "connect-src": ["'self'", externalURI, "https://*.strapi.io"],
+          "style-src": [
+            "'self'",
+            "'unsafe-inline'",
+            "'unsafe-eval'",
+            externalURI,
+            "https://fonts.googleapis.com",
+            "https://cdnjs.cloudflare.com",
+            "http://cdn.jsdelivr.net",
+          ],
+          "img-src": [
+            "'self'",
+            "blob:",
+            "data:",
+            externalURI,
+            "https://*.strapi.io",
             "https://m.media-amazon.com",
-            "https://tychr-strapi.s3.ap-south-1.amazonaws.com"
+            "https://tychr-strapi.s3.ap-south-1.amazonaws.com",
           ],
         },
       },
     },
   },
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "1024mb",
+      jsonLimit: "1024mb",
+      textLimit: "1024mb",
+    },
+  },
+  "strapi::favicon",
+  "strapi::public",
 ];
