cde8396bf084386c69bad32bb65c5696bf230e0b
Nabeel Rahman <mailnabeelrahman227@gmail.com>
2025-08-04 13:51:15 +0530

    Remove PDF invoice generation functionality and dependencies

--- src/api/payment/routes/razorpayment.js
+++ src/api/payment/routes/razorpayment.js
@@ -18,15 +18,6 @@ module.exports = {
         middlewares: [],
       },
     },
-    {
-      method: "GET",
-      path: "/payment/invoices/:id",
-      handler: "razorpayment.downloadInvoiceInstant",
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
   ],
   //   routes: [
   //   {
