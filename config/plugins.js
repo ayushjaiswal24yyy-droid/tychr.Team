6b7ca4b51d47ac17067630c5ec73a832683f838f
Kanish <k4ni5h23@gmail.com>
2026-05-07 05:19:16 +0530

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,35 +1,35 @@
 module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: env("AWS_REGION"),
-        params: {
-          ACL: env("AWS_ACL", "public-read"),
-          signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
-          Bucket: env("AWS_BUCKET_NAME"),
-        },
-      },
-      sizeLimit: 10 * 1024 * 1024 * 1024, // 1GB
+  // upload: {
+  //   config: {
+  //     provider: "aws-s3",
+  //     providerOptions: {
+  //       accessKeyId: env("AWS_ACCESS_KEY_ID"),
+  //       secretAccessKey: env("AWS_ACCESS_SECRET"),
+  //       region: env("AWS_REGION"),
+  //       params: {
+  //         ACL: env("AWS_ACL", "public-read"),
+  //         signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+  //         Bucket: env("AWS_BUCKET_NAME"),
+  //       },
+  //     },
+  //     sizeLimit: 10 * 1024 * 1024 * 1024, // 1GB
 
-      breakpoints: {
-        sm: 576,
-        md: 768,
-        lg: 992,
-        xl: 1200,
-      },
-      mimeTypes: [
-        "image/jpeg",
-        "image/png",
-        "image/gif",
-        "video/mp4",
-        "video/webm",
-        "video/ogg",
-      ],
-    },
-  },
+  //     breakpoints: {
+  //       sm: 576,
+  //       md: 768,
+  //       lg: 992,
+  //       xl: 1200,
+  //     },
+  //     mimeTypes: [
+  //       "image/jpeg",
+  //       "image/png",
+  //       "image/gif",
+  //       "video/mp4",
+  //       "video/webm",
+  //       "video/ogg",
+  //     ],
+  //   },
+  // },
   email: {
     config: {
       provider: "amazon-ses",
